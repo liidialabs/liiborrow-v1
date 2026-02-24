739d903054ce2f7ab5d5f22831ffa1aadb228426
calebomondi <kalebmokua@gmail.com>
2026-02-24 20:49:36 +0300

    updated to include to include mocks for testing

--- script/2_Supply.s.sol
+++ script/2_Supply.s.sol
@@ -6,33 +6,51 @@ import "forge-std/console2.sol";
 import { Aave } from  "../src/Aave.sol";
 import { DebtManager } from "../src/DebtManager.sol";
 import { UserCollateral, HealthStatus } from "../src/Types.sol";
+import { HelperConfig } from "./HelperConfig.s.sol";
+import { MockERC20 } from "../test/mocks/MockERC20.sol";
+import { MockAaveV3Pool } from "../test/mocks/MockAaveV3Pool.sol";
 
 /// @notice This script supplies ETH to the protocol
 contract Supply is Script {
     DebtManager private debtManager;
     Aave private aave;
+    HelperConfig public helperConfig;
+    MockERC20 public weth;
+    MockAaveV3Pool public mockPool;
 
-    address private DebtManagerAddress = 0x7EDcd4EC208E536e05aBE97213B54cb77E8007ce;
-    address private aaveAddress = 0x555d05ccf5590068679c07519445705f9f8CB62f;
-    address private WETH = 0xC558DBdd856501FCd9aaF1E62eae57A9F0629a3c; 
-    address private user = 0xC099f8A2C5117C81652A506aFfE10a6E77e79808; 
+    address private user; 
     
     uint256 private supplyAmount = 0.1 ether;
-    uint256 private deployerKey = vm.envUint("PRIVATE_KEY_USER");
+    uint256 private userKey = vm.envUint("PRIVATE_KEY_USER");
 
     function run() external {
-        // Initialize the DebtManager & Aave contract
-        debtManager = DebtManager(payable(DebtManagerAddress));
-        aave = Aave(aaveAddress);
+        // Deploy HelperConfig to get active network config
+        helperConfig = new HelperConfig();
+        (
+            ,, address _weth,,, address _pool,,,
+        ) = helperConfig.activeNetworkConfig();
 
-        vm.startBroadcast(deployerKey);
+        // user wallet
+        user = vm.addr(userKey);
 
-        // supply ETH to the protocol
-        debtManager.depositCollateralETH{value: supplyAmount}(supplyAmount);
+        // create contract instances
+        debtManager = DebtManager(payable(helperConfig.debtManagerAddress()));
+        aave = Aave(helperConfig.aaveAddress());
+        weth = MockERC20(_weth);
+        mockPool = MockAaveV3Pool(_pool);
+
+        vm.startBroadcast(userKey);
+
+        // mint WETH
+        weth.mint(user, supplyAmount);
+        // approve WETH to be spent by debt manager
+        weth.approve(address(debtManager), supplyAmount);
+        // supply WETH to the protocol
+        debtManager.depositCollateralERC20(_weth, supplyAmount);
 
         vm.stopBroadcast();
 
-        console2.log("Successfully supplied ETH to the protocol!");
+        console2.log("Successfully supplied WETH to the protocol!");
 
         // Get and log user's supplied collateral after supplying
         UserCollateral[] memory userCollateral = debtManager
@@ -54,7 +72,7 @@ contract Supply is Script {
             uint256 canBorrowUSDC,
             uint256 _currentLiquidationThreshold,
             uint256 _ltv
-        ) = aave.getUserAccountData(DebtManagerAddress);
+        ) = aave.getUserAccountData(address(debtManager));
 
         console2.log("Protocol's Aave position after supplying:");
         console2.log("Collateral in USD:", collateralUSD);
@@ -65,12 +83,12 @@ contract Supply is Script {
         console2.log("Loan to Value ratio:", _ltv);
 
         // Log protocol's health factor after supplying
-        (uint256 hf, HealthStatus status) = aave.getHealthFactor(DebtManagerAddress);
+        (uint256 hf, HealthStatus status) = aave.getHealthFactor(address(debtManager));
         console2.log("Protocol's health factor after supplying:", hf);
         console2.log("Protocol's health status after supplying:", uint256(status));
 
         // Log protocol's supply balance in WETH after supplying
-        uint256 supplyBalance = aave.getSupplyBalance(DebtManagerAddress, WETH);
+        uint256 supplyBalance = aave.getSupplyBalance(address(debtManager), address(weth));
         console2.log("Protocol's supply balance in WETH:", supplyBalance);
     }
 }
